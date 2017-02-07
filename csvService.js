b1039204ae9fa15de80ca9100c27aba21bfd8062
Andy <andrew.sunglae.kim@gmail.com>
2017-02-06 21:19:40 -0500

    encapsulates csv data into column arrays rather than row arrays

--- csvService.js
+++ csvService.js
@@ -6,7 +6,20 @@
   function Callback($http){
     console.log("service callback hit")
     $http.get('./random.csv').then((data)=>{
-      console.log(data)
-    })
+      let csvArray = data.data.trim().split('\n').map((rowStr) => {
+        return rowStr.split(',')
+      })
+      console.log(csvArray)
+      let colCsvArray = []
+      for(let i = 0; i < csvArray[0].length; i++){
+        colCsvArray.push([])
+      }
+      csvArray.forEach((row) => {
+        row.forEach((num, i) => {
+          colCsvArray[i].push(num)
+        })
+      })
+      console.log(colCsvArray)
+    })  
   }
 })()
